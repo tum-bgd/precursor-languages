9237652049c1a4fbdf90d797d96c642a054aeb67
Martin Werner <martin.werner@tum.de>
2024-08-23 07:17:00 +0200

    bootstrap flag corrected

--- plc/bootstrap-win.bat
+++ plc/bootstrap-win.bat
@@ -3,6 +3,6 @@ echo Generating Bootstrap
 echo Flexer
 winbison\win_flex.exe pl-compiler.l
 echo The YACC or Bison
-winbison\win_bison.exe pl-compiler.y
+winbison\win_bison.exe -d pl-compiler.y
 echo Now, you can:
 echo mkdir build // cd build // cmake .. // cmake --build --target=Release .
